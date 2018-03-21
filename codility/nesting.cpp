be7a00a18ec06174e82744d3663a9c23f7db6abc
Adegoke Obasa <adegokeobasa@gmail.com>
2018-03-21 16:33:17 +0100

    add: solution to nesting - lesson seven - codility

---
+++ codility/nesting.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Adegoke Obasa on 20/03/2018.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int solution(string &S) {
+    stack<char> characterStack;
+
+    unsigned long len = S.size();
+    if (len == 0) return 1;
+
+
+    for (int i = 0; (unsigned) i < len; i++) {
+        char c = S[i];
+        cout << c;
+    }
+
+    for (int i = 0; (unsigned) i < len; i++) {
+        char c = S[i];
+        if (c == '(') {
+            characterStack.push(c);
+        } else {
+            if (characterStack.empty()) return 0;
+            if (c == ')' && characterStack.top() != '(') return 0;
+            characterStack.pop();
+        }
+    }
+    return characterStack.empty() ? 1 : 0;
+}
+
+int main() {
+    cout << solution((string &) "(())") << endl;
+//    cout << solution((string &) "(())") << endl;
+//    cout << solution((string &) "(()") << endl;
+    return 0;
+}
